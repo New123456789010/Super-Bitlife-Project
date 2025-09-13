aaccd90903e8c68c01e1092c687ab591deddbadb
JM_Karin <jm_karin@outlook.com>
2025-09-13 21:22:58 +0700

    add dialogic timeline

--- ui-project/UI/gameplay.gd
+++ ui-project/UI/gameplay.gd
@@ -1,9 +1,26 @@
 extends Control
 
 @onready var control_2: Label = $Control2/HBoxContainer/MarginContainer/PanelContainer/MarginContainer/VBoxContainer/%Label2
+@onready var event_modal_panel: PanelContainer = $Control/EventModalPanel
 
+
+var timeline : DialogicTimeline = DialogicTimeline.new()
 var money: int
 
-func _on_button_pressed() -> void:
+func _ready():
+	Dialogic.signal_event.connect(_on_dialogic_signal)
+
+func _on_money_button_pressed() -> void:
 	money += 100
 	control_2.text = "%.2f" %money
+
+
+func _on_action_pressed() -> void:
+	if Dialogic.current_timeline != null:
+		return
+	else: 
+		Dialogic.start("finding_first_job")
+
+func _on_dialogic_signal(argument: String):
+	if argument == "show_newspaper_ads":
+		event_modal_panel.visible = true
