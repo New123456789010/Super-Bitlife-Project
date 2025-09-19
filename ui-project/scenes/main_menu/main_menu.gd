8eeeeb53d262488b90facefbca0c041fe6675dbc
Kevin99008 <salute.kevin99@gmail.com>
2025-09-19 13:21:55 +0700

    add bgm

--- ui-project/scenes/main_menu/main_menu.gd
+++ ui-project/scenes/main_menu/main_menu.gd
@@ -1,4 +1,6 @@
 extends Control
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+
 
 func _on_new_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/character_creation/character_creation.tscn")
