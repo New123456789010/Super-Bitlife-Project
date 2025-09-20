576d901efe673cb96930fdaa2391e159ef43af41
Kevin99008 <salute.kevin99@gmail.com>
2025-09-21 00:31:25 +0700

    combine character menu to main_menu, modify option menu, add button sound in gameplay menu

--- ui-project/scenes/components/option_menu.gd
+++ ui-project/scenes/components/option_menu.gd
@@ -1,5 +1,6 @@
 extends PanelContainer
 @onready var bgmvolume: HSlider = $PanelContainer/MarginContainer/VBoxContainer/VBoxContainer/BGMvolume
+@onready var option_menu: PanelContainer = $"."
 
 signal bgm_volume_changed(value: float)
 
@@ -7,7 +8,7 @@ func _ready() -> void:
 	bgmvolume.value_changed.connect(_on_slider_changed)
 	
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
+	option_menu.visible = false
 
 
 func _on_slider_changed(value: float) -> void:
--- ui-project/scenes/main_menu/main_menu.gd
+++ ui-project/scenes/main_menu/main_menu.gd
@@ -1,10 +1,17 @@
 extends Control
-@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var character_creation_menu: Control = $character_creation_menu
+@onready var start_menu: Control = $StartMenu
+@onready var option_menu: PanelContainer = $OptionMenu
 
 
 func _on_new_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/character_creation/character_creation.tscn")
+	start_menu.visible = false
+	character_creation_menu.visible = true
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_options_pressed() -> void:
+	option_menu.visible = true
