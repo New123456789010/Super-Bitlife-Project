bc41bd07bab993e2a376eb0516099879d8be05d4
Kevin99008 <salute.kevin99@gmail.com>
2025-09-21 00:36:18 +0700

    apply sfx button on mainmenu

--- ui-project/scenes/main_menu/main_menu.gd
+++ ui-project/scenes/main_menu/main_menu.gd
@@ -3,6 +3,18 @@ extends Control
 @onready var start_menu: Control = $StartMenu
 @onready var option_menu: PanelContainer = $OptionMenu
 
+func _ready():
+	_connect_buttons(self)
+
+func _connect_buttons(node: Node) -> void:
+	for child in node.get_children():
+		if child is Button:
+			child.pressed.connect(_on_any_button_pressed)
+		elif child.get_child_count() > 0:
+			_connect_buttons(child)
+
+func _on_any_button_pressed() -> void:
+	GlobalSound.play_click()
 
 func _on_new_game_pressed() -> void:
 	start_menu.visible = false
