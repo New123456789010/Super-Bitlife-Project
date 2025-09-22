dc8a02c0f725cd37972eed22fde1ca777d903539
JM_Karin <jm_karin@outlook.com>
2025-09-22 16:11:11 +0700

    map_scene instantiation fixed

--- ui-project/scenes/map_components/scene/map_screen.gd
+++ ui-project/scenes/map_components/scene/map_screen.gd
@@ -86,4 +86,4 @@ func _on_regenerate_button_pressed() -> void:
 
 
 func _on_back_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/gameplay/gameplay.tscn")
+	self.visible = !self.visible
